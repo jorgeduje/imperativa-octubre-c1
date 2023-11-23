df46e4c68c7b12739a1ad90450d24805071ae864
jorgeduje <jorgeduje4@gmail.com>
2023-11-23 20:02:28 -0300

    matrices 3

--- matrices.js
+++ matrices.js
@@ -146,3 +146,85 @@ const recorrerCompleta = mat => {
 
 recorrerCompleta(matrix5)
 
+
+// 
+
+let febrero = [
+  // EMPEZAREMOS A CONTAR LOS DIAS DESDE EL DIA LUNES
+  // EL MES ES FEBRERO QUE TIENE 31 DIAS
+  [1135, 2500, 900, 1600, 2800, 3650, 1100], // semana 1
+  [1750, 1890, 1900, 1300, 898, 1750, 2800], // semana 2
+  [1700, 1150, 1690, 1900, 1770, 4500, 2560], // semana 3
+  [800, 1250, 1430, 2100, 1980, 1270, 950], // semana 4
+];
+
+//TODO
+// CREANDO UNA FUNCION REUTILIZABLE...
+// CALCULAR GASTO TOTAL DE LA TERCER SEMANA
+
+const totalSemana = ( mes, numSemana )=>{
+
+    let posicionSemana = numSemana - 1
+
+    let contador = 0
+
+    for( let i = 0; i < mes[posicionSemana].length; i++ ){
+        contador += mes[posicionSemana][i]
+    }
+
+    return contador
+
+}
+
+let totalSemana3 = totalSemana( febrero, 3 )
+console.log(totalSemana3)
+
+
+//TODO
+// CREANDO UNA FUNCION REUTILIZABLE...
+// QUEREMOS SABER EL TOTAL DE LO QUE SE GASTO SUMANDO TOD´OS LOS DIAS SABADOS
+// ¿COMO LO HARIAS?
+
+const totalGastosDia = (mes, numDia )=>{
+    let posicionDia = numDia - 1
+    let acc = 0
+    for(let i = 0 ; i < mes.length; i++ ){
+        acc += mes[i][posicionDia]
+    }
+    return acc
+
+}
+
+let totalSabados = totalGastosDia(febrero, 6)
+console.log( totalSabados )
+
+//TODO
+// CALCULAR EL TOTAL GASTADO DEL MES
+
+const totalMensual = (mes)=>{
+
+    let acc = 0
+    for(let i = 0; i < mes.length; i++){
+
+        for(let j = 0 ; j < mes[i].length; j++ ){
+            acc += mes[i][j]
+        }
+
+    }
+
+    return acc
+}
+let totalFebrero = totalMensual(febrero)
+console.log(totalFebrero)
+
+
+
+
+
+
+// TODO
+// EN EL DIA QUE MAS SE GASTO, CUANTO FUE ?
+// EN QUE DIA Y SEMANA , SE PRODUJO ESE GASTO?
+
+
+
